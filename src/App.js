f827c3522316a7c77795964971893b9949a924c7
Gerardo <gerardo.domra@gmail.com>
2022-08-14 13:22:41 -0500

    Create header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src='https://upload.wikimedia.org/wikipedia/commons/4/47/Logo_del_ITESM.svg' className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './components.css'
+
+const Header = () => {
+    return (
+        <header>
+            <div className='header-box'>
+                <div className='header-items'>
+                    <div className='logo-box'>
+                        <div className='logo'>
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/4/47/Logo_del_ITESM.svg' alt='Logo ITESM'></img>
+                            <div className='logo-text'>
+                                <div className='logo-subtitle-text'>
+                                    <p>NOVUS</p>
+                                </div>
+                                <div className='logo-title-text'>
+                                    <p>Asesor Inteligente</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <nav className='nav-box'>
+                        <div className='header-breadcrumb'>
+                            <p>Lecciones</p>
+                        </div>
+                        <div className='header-breadcrumb'>
+                            <p>Ejercicios</p>
+                        </div>
+                    </nav>
+                    <div className='login-nav-box'>
+                        <div className='login'>
+                            Ingresar
+                        </div>
+                        <div className='register'>
+                            Registrarse
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
