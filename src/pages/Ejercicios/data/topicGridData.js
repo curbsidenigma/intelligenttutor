114eebb2b02575eae10a448815bdbb019eea8e7d
Gerardo <gerardo.domra@gmail.com>
2022-09-16 14:10:09 -0500

    Add responsive grid in 'Ejercicios' section

---
+++ src/pages/Ejercicios/data/topicGridData.js
@@ -0,0 +1,36 @@
+const topicGridData = [
+    {
+        id: 1,
+        link: '/intelligenttutor/ejercicios/cinematica-dinamica-mecanismos/',
+        img: 'https://cdn.stocksnap.io/img-thumbs/960w/still-items_LCLKTHHCMX.jpg',
+        alt: 'Mecanismos de cuatro barras',
+        title: 'Análisis de Mecanismos',
+        subtitle: 'Mecanismos de cuatro barras'
+    },
+    {
+        id: 2,
+        link: '/intelligenttutor/404/',
+        img: 'https://cdn.stocksnap.io/img-thumbs/960w/metal-tools_VEFWNXZWZY.jpg',
+        alt: 'Concepto de esfuerzo',
+        title: 'Mecánica de Materiales',
+        subtitle: 'Concepto de esfuerzo'
+    },
+    {
+        id: 3,
+        link: '/intelligenttutor/404/',
+        img: 'https://cdn.stocksnap.io/img-thumbs/960w/steel-industry_1PR1SGKS69.jpg',
+        alt: 'Fundición',
+        title: 'Procesos de Manufactura',
+        subtitle: 'Fundición'
+    },
+    {
+        id: 4,
+        link: '/intelligenttutor/404/',
+        img: 'https://cdn.stocksnap.io/img-thumbs/960w/circuit-chip_1KREEXFTZT.jpg',
+        alt: 'Leyes de Kirchhoff',
+        title: 'Circuitos Eléctricos',
+        subtitle: 'Leyes de Kirchhoff'
+    }
+]
+
+export default topicGridData
