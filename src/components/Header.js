eb2af8376340e4188fbd716803b0acb30b7ebbcb
Gerardo <gerardo.domra@gmail.com>
2022-09-16 11:49:48 -0500

    Set navbar as independent element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import '../styles/components.css'
 import Logo from './Logo'
+import Navbar from './Navbar'
 
 const Header = () => {
     return (
@@ -10,28 +10,8 @@ const Header = () => {
                 <div className='logo-box'>
                     <Logo />
                 </div>
-                <div className='header-items'>
-                    <nav>
-                        <div className='nav-box'>
-                            <Link to='/intelligenttutor/404/' className='header-breadcrumb'>
-                                Lecciones
-                            </Link>
-                            <Link to='/intelligenttutor/ejercicios/' className='header-breadcrumb'>
-                                Ejercicios
-                            </Link>
-                            <Link to='/intelligenttutor/404/' className='header-breadcrumb'>
-                                Información
-                            </Link>
-                        </div>
-                        <div className='login-nav-box'>
-                            <Link to='/intelligenttutor/ingresar/' className='login'>
-                                Ingresar
-                            </Link>
-                            <Link to='/intelligenttutor/registrarse' className='register'>
-                                Registrarse
-                            </Link>
-                        </div>
-                    </nav>
+                <div className='navbar-box'>
+                    <Navbar />
                 </div>
             </div>
         </header>
---
+++ src/data/navbarData.js
@@ -0,0 +1,19 @@
+const navbarData = [
+    {
+        id: 1,
+        name: 'Lecciones',
+        link: '/intelligenttutor/404/'
+    },
+    {
+        id: 2,
+        name: 'Ejercicios',
+        link: '/intelligenttutor/ejercicios/'
+    },
+    {
+        id: 3,
+        name: 'Información',
+        link: '/intelligenttutor/404/'
+    },
+]
+
+export default navbarData
