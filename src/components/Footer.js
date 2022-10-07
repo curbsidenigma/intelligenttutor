45dea83f6dc998628ae4087282902f6239fc43b9
Gerardo Domínguez <gerardo.domra@gmail.com>
2022-10-06 23:43:04 -0500

    Set reactive header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/components.css'
+import '../styles/footer.css'
 import Logo from './Logo'
 
 const Footer = () => {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/components.css'
-import navbarData from '../data/navbarData'
+import '../styles/header.css'
+
+const navbarDataLeft = [
+    {
+        id: 1,
+        name: 'Lecciones',
+        link: '/intelligenttutor/404/',
+        class: 'Header-link'
+    },
+    {
+        id: 2,
+        name: 'Ejercicios',
+        link: '/intelligenttutor/ejercicios/',
+        class: 'Header-link'
+    },
+    {
+        id: 3,
+        name: 'Información',
+        link: '/intelligenttutor/404/',
+        class: 'Header-link'
+    }
+]
+
+const navbarDataRight = [
+    {
+        id: 4,
+        name: 'Ingresar',
+        link: '/intelligenttutor/ingresar/',
+        class: 'Header-link'
+    },
+    {
+        id: 5,
+        name: 'Registrarse',
+        link: '/intelligenttutor/registrarse/',
+        class: 'Header-Register-link'
+    }
+]
 
 const Navbar = () => {
     return (
-        <nav>
-            <div className='nav-box'>
-                {navbarData.map(data => (
-                    <Link key={data.id} to={data.link} className='header-breadcrumb'>
-                        {data.name}
-                    </Link>
-                ))}
-            </div>
-            <div className='login-nav-box'>
-                <Link to='/intelligenttutor/ingresar/' className='login'>
-                    Ingresar
+        <>
+            {navbarDataLeft.map(element => (
+                <Link key={element.id} to={element.link} className={element.class}>
+                    {element.name}
                 </Link>
-                <Link to='/intelligenttutor/registrarse' className='register'>
-                    Registrarse
+            ))}
+            <div className='Header-break'></div>
+            {navbarDataRight.map(element => (
+                <Link key={element.id} to={element.link} className={element.class}>
+                    {element.name}
                 </Link>
-            </div>
-        </nav>
+            ))}
+        </>
     )
 }
 
